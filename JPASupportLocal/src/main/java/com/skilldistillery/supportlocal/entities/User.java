6fc5f5d1b2ab5f1d3ae664590668447229fb7f6c
Ankitshah89 <shahdankit@gmail.com>
2020-04-28 12:00:39 -0600

    working unit tests

--- JPASupportLocal/src/main/java/com/skilldistillery/supportlocal/entities/User.java
+++ JPASupportLocal/src/main/java/com/skilldistillery/supportlocal/entities/User.java
@@ -1,5 +1,51 @@
 package com.skilldistillery.supportlocal.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public User() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + "]";
+	}
+	
+	
+	
+	
+	
+	
 
 }
