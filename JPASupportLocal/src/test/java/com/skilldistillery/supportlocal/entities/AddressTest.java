a4bbbf9813d2bc4c7213e3f3f52eb2d9917d7ebc
KerwinJT <kerwinjt@gmail.com>
2020-04-28 14:16:34 -0600

    Committing to be able to pull new database

---
+++ JPASupportLocal/src/test/java/com/skilldistillery/supportlocal/entities/AddressTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.supportlocal.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("SupportLocalPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		address = null;
+	}
+
+	@Test
+	@DisplayName("Address testing without relationship")
+	void test() {
+		assertNotNull(address);
+		assertEquals("123 Street", address.getStreet());
+	}
+
+}
