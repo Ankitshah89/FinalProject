565d06fcdd8b63ca285419add51529c391dc6aa4
Devin Cooper <devinraycooper@gmail.com>
2020-04-29 15:47:50 -0600

    Finished with CRD operations of Article. Need to add search by specific options and double check access rights

--- SupportLocal/src/main/java/com/skilldistillery/supportlocal/repositories/BusinessRepository.java
+++ SupportLocal/src/main/java/com/skilldistillery/supportlocal/repositories/BusinessRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.supportlocal.entities.Business;
+import com.skilldistillery.supportlocal.entities.User;
 
 public interface BusinessRepository extends JpaRepository<Business, Integer> {
+	Business findByManager(User user);
 	List<Business> findByName(String name);
 	List<Business> findByDescription(String description);
 	List<Business> findByAddressPostalCode(String zip);
