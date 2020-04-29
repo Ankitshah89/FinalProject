9c5069950c5714154331e9ff65eccac461031043
Ankitshah89 <shahdankit@gmail.com>
2020-04-29 17:30:22 -0600

    review comment services updates

--- JPASupportLocal/src/main/java/com/skilldistillery/supportlocal/entities/ReviewComment.java
+++ JPASupportLocal/src/main/java/com/skilldistillery/supportlocal/entities/ReviewComment.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.supportlocal.entities;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -34,54 +35,120 @@ public class ReviewComment {
 	private String content;
 
 	@Column(name = "create_date")
-	private LocalDate createdAt;
+	private LocalDateTime createdAt;
+	
+	@Column(name = "date_updated")
+	private LocalDateTime updatedAt;
 	
 	
 	@ManyToOne
 	@JoinColumn(name = "inreply_to_id")
 	private ReviewComment parentComment;
+	
+	private boolean active;
 
-	/////////////////
 
 	public int getId() {
 		return id;
 	}
 
+
+	public ReviewComment(int id, Review review, User user, String content, LocalDateTime createdAt,
+			LocalDateTime updatedAt, ReviewComment parentComment, Boolean active) {
+		super();
+		this.id = id;
+		this.review = review;
+		this.user = user;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.parentComment = parentComment;
+		this.active = active;
+	}
+
+
+	public ReviewComment() {
+		super();
+	}
+
+
+	public ReviewComment(int id, Review review, User user, String content, LocalDateTime createdAt,
+			LocalDateTime updatedAt, ReviewComment parentComment) {
+		super();
+		this.id = id;
+		this.review = review;
+		this.user = user;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.parentComment = parentComment;
+	}
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 
 	public Review getReview() {
 		return review;
 	}
 
+
 	public void setReview(Review review) {
 		this.review = review;
 	}
+
 
 	public User getUser() {
 		return user;
 	}
 
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 
 	public String getContent() {
 		return content;
 	}
 
+
 	public void setContent(String content) {
 		this.content = content;
 	}
 
-	public LocalDate getCreatedAt() {
+
+	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
 
-	public void setCreatedAt(LocalDate createdAt) {
+
+	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
+
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+	public ReviewComment getParentComment() {
+		return parentComment;
+	}
+
+
+	public void setParentComment(ReviewComment parentComment) {
+		this.parentComment = parentComment;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -90,10 +157,13 @@ public class ReviewComment {
 		result = prime * result + ((content == null) ? 0 : content.hashCode());
 		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
 		result = prime * result + id;
+		result = prime * result + ((parentComment == null) ? 0 : parentComment.hashCode());
 		result = prime * result + ((review == null) ? 0 : review.hashCode());
+		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
 		result = prime * result + ((user == null) ? 0 : user.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -116,10 +186,20 @@ public class ReviewComment {
 			return false;
 		if (id != other.id)
 			return false;
+		if (parentComment == null) {
+			if (other.parentComment != null)
+				return false;
+		} else if (!parentComment.equals(other.parentComment))
+			return false;
 		if (review == null) {
 			if (other.review != null)
 				return false;
 		} else if (!review.equals(other.review))
+			return false;
+		if (updatedAt == null) {
+			if (other.updatedAt != null)
+				return false;
+		} else if (!updatedAt.equals(other.updatedAt))
 			return false;
 		if (user == null) {
 			if (other.user != null)
@@ -129,31 +209,24 @@ public class ReviewComment {
 		return true;
 	}
 
+
 	@Override
 	public String toString() {
-		return "ReviewComment [id=" + id + ", review=" + review + ", user=" + user + ", content=" + content
-				+ ", createdAt=" + createdAt + "]";
+		return "ReviewComment [id=" + id + ", content=" + content + ", createdAt=" + createdAt + ", updatedAt="
+				+ updatedAt + ", parentComment=" + parentComment + ", active=" + active + "]";
 	}
 
-	public ReviewComment(int id, Review review, User user, String content, LocalDate createdAt) {
-		super();
-		this.id = id;
-		this.review = review;
-		this.user = user;
-		this.content = content;
-		this.createdAt = createdAt;
+
+	public boolean isActive() {
+		return active;
 	}
 
-	public ReviewComment() {
-		super();
+
+	public void setActive(boolean active) {
+		this.active = active;
 	}
 
-	public ReviewComment getParentComment() {
-		return parentComment;
-	}
 
-	public void setParentComment(ReviewComment parentComment) {
-		this.parentComment = parentComment;
-	}
+	
 
 }
--- SupportLocal/src/main/java/com/skilldistillery/supportlocal/services/ReviewCommentImpl.java
+++
@@ -1,5 +0,0 @@
-package com.skilldistillery.supportlocal.services;
-
-public class ReviewCommentImpl implements ReviewComment {
-
-}
