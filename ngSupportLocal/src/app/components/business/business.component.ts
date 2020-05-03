fcdeeb6f0b22f6d1ad1662d2dcb43ca9372fcc6e
Devin Cooper <devinraycooper@gmail.com>
2020-05-02 21:36:42 -0600

    Finished mockup of business profile page

--- ngSupportLocal/src/app/components/business/business.component.ts
+++ ngSupportLocal/src/app/components/business/business.component.ts
@@ -9,4 +9,29 @@ export class BusinessComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  myFunction() {
+    var dots = document.getElementById('dots');
+    var btnText = document.getElementById('myBtn');
+
+    if (dots.style.display === '-webkit-box') {
+      dots.style.display = 'inline';
+      btnText.innerHTML = 'Read more';
+    } else {
+      dots.style.display = '-webkit-box';
+      btnText.innerHTML = 'Read less';
+    }
+  }
+  showComments() {
+    var commentBox = document.getElementById('commentDiv');
+    var btnText = document.getElementById('divBtn');
+
+    if (commentBox.style.display === 'none') {
+      commentBox.style.display = 'block';
+      btnText.innerHTML = 'Hide Comments';
+    } else {
+      commentBox.style.display = 'none';
+      btnText.innerHTML = 'Show Comments';
+    }
+  }
 }
