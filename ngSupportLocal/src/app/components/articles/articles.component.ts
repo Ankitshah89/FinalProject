cde558139f95a8ebd0f32cfd0e8b79d3a396e120
Devin Cooper <devinraycooper@gmail.com>
2020-05-04 22:58:07 -0600

    changes

--- ngSupportLocal/src/app/components/articles/articles.component.ts
+++ ngSupportLocal/src/app/components/articles/articles.component.ts
@@ -16,64 +16,47 @@ import { Article } from 'src/app/models/article';
 })
 export class ArticlesComponent implements OnInit {
   articleList: Article[] = [];
-  selected : Article;
+  selected: Article;
   keyword: string = null;
   list = false;
   rating = 0;
   averageRating = 0;
 
-
   articleComments: ArticleComment[];
 
-  constructor(
-    private articleSvc: ArticleService,
-    private router: Router,
-    private userSvc: UserService,
-    private review: ReviewService,
-    private business: BusinessService,
-    private authService: AuthService
-  ) {}
+  constructor(private articleSvc: ArticleService) {}
 
   categories = ['All', 'Sports', 'Entertainment', 'Shopping'];
 
   selectedType = 'All';
 
   ngOnInit(): void {
-    this.selected= null;
+    this.selected = null;
     this.loadArticles();
-
-
   }
 
   loadArticles() {
     this.articleList = [];
-    this.articleSvc.index().subscribe(
-      (good) => {
+    this.articleSvc.index().subscribe((good) => {
       good.forEach((article) => {
         if (article.active) {
           this.articleList.push(article);
         }
       });
-      (bad) =>{
-        console.log('didntWork')
-      }
+      (bad) => {
+        console.log('didntWork');
+      };
     });
   }
 
-   displayArticleComments(article : Article){
-     this.selected = article;
-     this.articleSvc.selected = article;
-     this.articleSvc.loadArticleComments().subscribe(
-       good => {
+  displayArticleComments(article: Article) {
+    this.selected = article;
+    this.articleSvc.selected = article;
+    this.articleSvc.loadArticleComments().subscribe(
+      (good) => {
         this.articleComments = good;
-       },
-       bad =>{
-
-       }
-     )
-
-
-
+      },
+      (bad) => {}
+    );
   }
-
 }
